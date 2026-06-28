a8e91403f495b46a610643706bbbe7d9bc1d20d9
Rawan Essam <rawanessam253@gmail.com>
2026-06-28 18:51:24 +0300

    fix: update pydantic schema for words

--- app/schemas.py
+++ app/schemas.py
@@ -1,10 +1,12 @@
-from pydantic  import BaseModel,Field
+from pydantic import BaseModel,Field
 from typing import Optional
 class wordbase(BaseModel):
     word:str=Field(...,min_length=2,max_length=100)
     definition:str=Field(...,min_length=3,max_length=300)
-    example:Optional[str]=Field(None,max_length=300)
+    example: Optional[str] = Field(default=None, max_length=300)
 class wordcreate(wordbase):
     pass
 class wordresponse(wordbase):
     id:int
+    class Config:
+        from_attributes = True
